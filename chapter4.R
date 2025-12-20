de2da38cc6ce272afc6482d272b71837ebfd2274
phamhaan1987 <phamhaan1987@gmail.com>
2025-12-20 18:17:11 +0700

    Updated chapter4.R

--- chapter4.R
+++ chapter4.R
@@ -42,7 +42,7 @@ quux
 any(quux)
 all(quux)
 
-# Exercises #
+## Exercises ##
 a_vec <- c(6,9,7,3,6,7,9,6,3,6,6,7,1,9,1)
 a_vec == 6
 a_vec >= 6
@@ -58,3 +58,93 @@ all(b_array <= (6/2+4))
 any(b_array+2 <= (6/2+4))
 all(b_array+2 <= (6/2+4))
 any(diag(x=(diag(10) == 0)))
+
+### Multiple Comparisons: Logical Operators
+FALSE || ((T&&TRUE)||FALSE)     # &&, ||: single logical version
+!TRUE&&T
+(T&&(TRUE||F))&&FALSE
+(6<4) || (3!=1)
+foo <- c(T,F,F,F,T,F,T,T,T,F,T,F)
+foo
+bar <- c(F,T,F,T,F,F,F,F,T,T,T,T)
+bar
+foo & bar   # element-wise logical version
+foo | bar
+foo && bar  # Error
+foo || bar  # Error
+
+## Exercises ##
+foo <- c(7,1,7,10,5,9,10,3,10,8)
+(foo > 5) | (foo == 2)
+bar <- c(8,8,4,4,5,1,5,6,6,8)
+(bar <= 6) & (bar != 4)
+((foo > 5) | (foo == 2)) & ((bar <= 6) & (bar != 4))
+baz <- foo+bar
+baz
+(baz >= 14) & (baz != 15)
+(baz/foo > 4) | (baz/foo <= 2)
+(foo > 5) || (foo == 2)     # Error
+
+### Logicals Are Numbers!
+TRUE + TRUE # In elementary numeric operations, TRUE is treated like 1, and FALSE is treated like 0
+FALSE - TRUE
+T+T+F+T+F+F+T
+1 && 1
+1 && 0
+0 && 1
+0 && 0
+1 || 0
+0 || 1
+2 && 3  # by contrast, in logical operations, number not 0 is treated by TRUE, 0 is FALSE
+2 || 3
+2 && 0
+2 || 0
+0.2 && -0.5
+
+### Logical Subsetting and Extraction
+myvec <- c(5,-2.3,4,4,4,6,8,10,40221,-8)
+myvec[c(F,T,F,F,F,F,F,F,F,T)]
+myvec < 0
+myvec[myvec<0]
+myvec[c(T,F)]
+myvec[(myvec>0) & (myvec<1000)]
+myvec[myvec<0] <- -200
+myvec
+which(x=c(T,F,F,T,T))
+which(x=myvec<0)
+myvec[-which(myvec<0)]
+A <- matrix(c(0.3,4.5,55.3,91,0.1,105.5,-4.2,8.2,27.9),nrow=3,ncol=3)
+A
+A[c(T,F,F), c(F,T,T)]   # the same with A[1, 2:3]
+A < 1
+A[A<1]
+A[A<1] <- -7
+A
+A > 25
+which(x=A>25)
+which(c(A[,1],A[,2],A[,3]) > 25)    # the same with which(x=A>25)
+which(x=A>25, arr.ind=FALSE)    # by default, arr.ind=FALSE
+which(x=A>25, arr.ind=T)
+
+## Exercises ##
+foo <- c(7,5,6,1,2,10,8,3,8,2)
+foo
+bar <- foo[foo>=5]
+bar
+foo[-which(foo>=5)]
+baz <- matrix(data=bar, nrow=2, ncol=3, byrow=TRUE)
+baz
+baz[baz==8] <- baz[1,2]^2
+baz
+all((baz<=25) & (baz>4))
+qux <- array(data=c(10,5,1,4,7,4,3,3,1,3,4,3,1,7,8,3,7,3), dim=c(3,2,3))
+qux
+which((qux==3) | (qux==4), arr.ind=TRUE)
+qux
+qux[(qux<3) | (qux==7)] <- 100
+qux
+foo
+foo[c(F,T)]
+foo[c(0,1)]
+
+## Characters
