47a4f6fc1dc2ad8cf735186d018b880b5be273d2
phamhaan1987 <phamhaan1987@gmail.com>
2025-12-26 00:08:38 +0700

    Updated chapter7.R

--- chapter7.R
+++ chapter7.R
@@ -34,3 +34,71 @@ plot(foo, bar, main="My lovely plot", xlab="x axis label", ylab="location y")   
 plot(foo, bar, type="b", main="My lovely plot\ntitle on two lines", xlab="", ylab="")
 
 ### Color
+colors()    # number of uniquely named colors
+plot(foo, bar, type="b", main="My lovely plot", xlab="", ylab="", col=2)    # by default, col=1 (~black)
+plot(foo, bar, type="b", main="My lovely plot", xlab="", ylab="", col="seagreen4")  # There are eight possible integer values and over 650 character strings to specify color
+plot(foo, bar, type="b", main="My lovely plot", xlab="", ylab="",
+    col=4, pch=8, lty=2, cex=2.3, lwd=3.3)  # The pch parameter controls the character to plot individual data points, a value between 1 and 25
+plot(foo, bar, type="b", main="My lovely plot", xlab="", ylab="",
+    col=6, pch=15, lty=3, cex=0.7, lwd=2)   # The lty parameter, which affects the type of line drawn, can take the values 1 through 6
+
+### Custom Region Limits
+plot(foo, bar, type="b", main="My lovely plot", xlab="", ylab="",
+    col=4, pch=8, lty=2, cex=2.3, lwd=3.3, xlim=c(-10,5), ylim=c(-3,3))
+plot(foo, bar, type="b", main="My lovely plot", xlab="", ylab="",
+    col=6, pch=15, lty=3, cex=0.7, lwd=2, xlim=c(3,5), ylim=c(-0.5,0.2))
+
+### Aspect Ratio
+plot(foo, bar, pch=19)
+plot(foo, bar, pch=19, asp=1)
+
+## Adding Points, Lines, and Text to an Existing Plot
+x <- 1:20
+y <- c(-1.49,3.37,2.59,-2.78,-3.94,-0.92,6.43,8.51,3.41,-8.23,
+    -12.01,-6.58,2.87,14.12,9.63,-4.58,-14.78,-11.67,1.17,15.62)
+
+plot(x, y, type="n", main="")
+abline(h=c(-5,5), col="red", lty=2, lwd=2)
+segments(x0=c(5,15), y0=c(-5,-5), x1=c(5,15), y1=c(5,5), col="red", lty=3, lwd=2)
+points(x[y>=5],y[y>=5], pch=4, col="darkmagenta", cex=2)
+points(x[y<=-5],y[y<=-5], pch=3, col="darkgreen", cex=2)
+points(x[(x>=5&x<=15)&(y>-5&y<5)], y[(x>=5&x<=15)&(y>-5&y<5)], pch=19, col="blue")
+points(x[(x<5|x>15)&(y>-5&y<5)], y[(x<5|x>15)&(y>-5&y<5)])
+lines(x, y , lty=4)
+arrows(x0=8, y0=14, x1=11, y1=2.5)
+text(x=8, y=15, labels="sweet spot")
+legend(
+    "bottomleft",
+    legend=c("overall process","sweet","standard","too big","too small","sweet y range","sweet x range"),
+    pch=c(NA,19,1,4,3,NA,NA),
+    lty=c(4,NA,NA,NA,NA,2,3),
+    col=c("black","blue","black","darkmagenta","darkgreen","red","red"),
+    lwd=c(1,NA,NA,NA,NA,2,2),
+    pt.cex=c(NA,1,1,2,2,NA,NA)
+)
+
+## Exercises ##
+?plot.window
+plot(x=NULL, y=NULL, type="n", xlab="", ylab="", xlim=c(-3,3), ylim=c(7,13))
+abline(h=c(7,13), v=c(-3,3), col="grey", lty=2, lwd=5)
+text(x=0, y=10, labels="SOMETHING\nPROFOUND", cex=2)
+arrows(x0=-2.5, y0=12.5, x1=-1, y1=10.5)
+arrows(x0=-2.5, y0=10, x1=-1, y1=10)
+arrows(x0=-2.5, y0=7.5, x1=-1, y1=9.5)
+arrows(x0=2.5, y0=12.5, x1=1, y1=10.5)
+arrows(x0=2.5, y0=10, x1=1, y1=10)
+arrows(x0=2.5, y0=7.5, x1=1, y1=9.5)
+
+weight <- c(55,85,75,42,93,63,58,75,89,67)
+height <- c(161,185,174,154,188,178,170,167,181,178)
+sex <- c("Female","Male","Male","Female","Male","Male","Female","Male","Male","Female")
+plot(weight, height, type="n", main="Weight and Height by Sex", xlab="Weight (kg)", ylab="Height (cm)")
+points(weight[sex=="Female"], height[sex=="Female"], col="red", pch=1, cex=2)
+points(weight[sex=="Male"], height[sex=="Male"], col="green", pch=2, cex=2)
+legend(
+    "bottomleft",
+    legend=c("Female","Male"),
+    col=c("red","green"),
+    pch=c(1,2),
+    pt.cex=c(2,2)
+)
